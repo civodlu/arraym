c9521831d303c13afe9b8868e697744d4b79bdb3
civodlu <civodlu@gmail.com>
2016-11-23 20:30:23 -0500

    wip

--- array/array-op-impl.h
+++ array/array-op-impl.h
@@ -1,5 +1,15 @@
 #pragma once
 
+/**
+ @file
+
+ This file is implementing building blocks operation for arrays and in particular using BLAS as much as possible
+ */
 DECLARE_NAMESPACE_NLL
+
+namespace details
+{
+
+}
 
 DECLARE_NAMESPACE_END
